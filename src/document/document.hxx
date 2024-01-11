0f3c7eb0097ea9ccc5a0ff2e3721fe9c63d66557
DarenKostov <daren.kostov@outlook.com>
2024-01-10 17:01:46 -0800

    smalttext hmmmmm

--- src/document/document.hxx
+++ src/document/document.hxx
@@ -1,17 +1,17 @@
 /*
 Copyright (c) 2024 Daren Kostov
 
-This file is part of smalt
+This file is part of smalttext
 
-smalt is free software: you can redistribute it and/or modify it under the terms
+smalttext is free software: you can redistribute it and/or modify it under the terms
 of the GNU General Public License as published by the Free Software Foundation,
 either version 3 of the License, or (at your option) any later version.
 
-smalt is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+smalttext is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with smalt
+You should have received a copy of the GNU General Public License along with smalttext
 If not, see <https://www.gnu.org/licenses/>.
 */
 
